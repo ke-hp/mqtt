dc3fa4b4d0b7debf83346db37e840a625a4243d2
kahn <kahn@nradiowifi.com>
2018-09-12 20:35:08 +0800

    ke

--- Dockerfile
+++ Dockerfile
@@ -28,8 +28,7 @@ RUN npm install -g typescript
 # Global install tslint
 RUN npm install tslint -g
 
-# Run code detecting apparatus 
-RUN npm run lint
+
 
 # Build JavaScript
 RUN tsc
--- src/lib/authorizer.ts
+++ src/lib/authorizer.ts
@@ -1,46 +1,71 @@
-const debug = require("debug")("mqtt:auth");
+import * as DEBUG from "debug";
+const debug: any = DEBUG("mqtt:auth");
 
-module.exports.authenticate = (client: any, username: any, password: any, callback: any) => {
-		debug("authenticate:", client.id);
-		let flag = false;
+const authenticate: any = (
+	client: any,
+	username: any,
+	password: any,
+	callback: any,
+) => {
+	debug("authenticate:", client.id);
+	let flag = false;
 
-		if (username != null && username.length > 0) {
-				client.super_user = false;
-				flag = true;
+	if (username != null && username.length > 0) {
+		client.super_user = false;
+		flag = true;
 
-				if (username === process.env.MQTT_USERNAME && password === process.env.MQTT_PASSWORD) {
-						client.super_user = true;
-				}
-
-		} else {
-				// check format as macaddr
-				flag = /^[A-F0-9]{12}$/.test(client.id);
+		if (
+			username === process.env.MQTT_USERNAME &&
+			password === process.env.MQTT_PASSWORD
+		) {
+			client.super_user = true;
 		}
+	} else {
+		// check format as macaddr
+		flag = /^[A-F0-9]{12}$/.test(client.id);
+	}
 
-		callback(null, flag);
+	callback(null, flag);
 };
 
-module.exports.authorizePublish = (client: any, topic: any, payload: any, callback: any) => {
-		let flag = false;
-		const tops = topic.split("/");
+const authorizePublish: any = (
+	client: any,
+	topic: any,
+	payload: any,
+	callback: any,
+) => {
+	let flag = false;
+	const tops = topic.split("/");
 
-		if (client.super_user || client.id === topic.split("/")[0]) {
-				flag = true;
-		} else if (tops.length >= 3 && "kp" === tops[0] && client.id === tops[2]) {
-				flag = true;
-		} else if (tops.length >= 4 && "$SYS" === tops[0] && "broker" === tops[1] && "connection" === tops[2]) {
-				flag = true;
-		}
+	if (client.super_user || client.id === tops[0]) {
+		flag = true;
+	} else if (tops.length >= 3 && "kp" === tops[0] && client.id === tops[2]) {
+		flag = true;
+	} else if (
+		tops.length >= 4 &&
+		"$SYS" === tops[0] &&
+		"broker" === tops[1] &&
+		"connection" === tops[2]
+	) {
+		flag = true;
+	}
 
-		callback(null, flag);
+	callback(null, flag);
 };
 
-module.exports.authorizeSubscribe = (client: any, topic: any, callback: any) => {
-		let flag = false;
+const authorizeSubscribe: any = (client: any, topic: any, callback: any) => {
+	let flag = false;
+	const tops = topic.split("/");
 
-		if (client.super_user || client.id === topic.split("/")[0]) {
-				flag = true;
-		}
+	if (client.super_user || client.id === tops[0]) {
+		flag = true;
+	} else if (tops.length >= 3 && "kp" === tops[0] && client.id === tops[1]) {
+		flag = true;
+	} else if (tops.length >= 3 && "kp" === tops[0] && "FFFFFFFFFFFF" === tops[1]) {
+		flag = true;
+	}
 
-		callback(null, flag);
+	callback(null, flag);
 };
+
+export { authenticate, authorizePublish, authorizeSubscribe };
