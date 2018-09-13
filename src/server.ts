255e4c739a249059c82571115d1c9ec8be96676d
kahn <kahn@nradiowifi.com>
2018-09-13 20:45:12 +0800

    ke

--- src/server.ts
+++ src/server.ts
@@ -41,14 +41,12 @@ server.on("clientConnected", (client: any) => {
 	publish(client.id, true);
 	if ( !isNull(process.env.MQTT_BACKWARD_TOPIC) && !isNull(process.env.MQTT_BACKWARD_COMMAD)) {
 		if (/^[A-F0-9]{12}$/.test(client.id)) {
-		// if (/^[A-F0-9]{12}$/.test(client.id) && topic === process.env.MQTT_BACKWARD_TOPIC) {
 			const backwardCommad: string[] = process.env.MQTT_BACKWARD_COMMAD.split("/");
 			let topic = process.env.MQTT_BACKWARD_TOPIC;
 			const tops: string[] = process.env.MQTT_BACKWARD_TOPIC.split("/");
 			if (tops[1] === "CLIENT_ID") {
 				topic = process.env.MQTT_BACKWARD_TOPIC.replace(/CLIENT_ID/g, client.id);
 			}
-			console.log("2222222222222", topic);
 			backwardCommad.forEach((payloadValue: any) => {
 				const message: any = {
 					topic,
@@ -57,11 +55,10 @@ server.on("clientConnected", (client: any) => {
 					retain: false,
 				};
 				setTimeout(() => {
-					console.log("33333333333333", payloadValue);
 					server.publish(message, () => {
 						debug("onl:cmd: done!");
 					});
-				}, 5000);
+				}, Math.floor(Math.random() * (19) + 10));
 			},
 		);
 	}
