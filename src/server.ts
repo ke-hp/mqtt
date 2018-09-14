b1d497e038d7b63aa1b6a1dd44d1c565be564f51
kahn <kahn@nradiowifi.com>
2018-09-14 19:53:57 +0800

    ke

--- src/server.ts
+++ src/server.ts
@@ -34,6 +34,7 @@ const server = new mosca.Server(moscaSetting);
 
 // method
 server.on("clientConnected", (client: any) => {
+	console.log("设备连接");
 	debug("onl:", client.id);
 	publish(client.id, true);
 });
